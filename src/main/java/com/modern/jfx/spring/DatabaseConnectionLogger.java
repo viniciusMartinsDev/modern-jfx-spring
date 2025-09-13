16944b5e3ef5f139b733d55e40e8e695c4d51b5c
Vinícius Martins <vinicius.martins.developer@gmail.com>
2025-09-13 16:32:59 -0300

    feat: implementa classe de logger para o banco de dados

---
+++ src/main/java/com/modern/jfx/spring/DatabaseConnectionLogger.java
@@ -0,0 +1,23 @@
+package com.modern.jfx.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseConnectionLogger implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... args){
+        try {
+            jdbcTemplate.execute("SELECT 1");
+            System.out.println("✅ Conexão com o banco de dados estabelecida com sucesso!");
+        } catch (Exception e) {
+            System.err.println("❌ Falha ao conectar ao banco de dados: " + e.getMessage());
+        }
+    }
+}
