22d1d6e6c6768def7ac799573d3c3b172faa1009
Vinícius Martins <vinicius.martins.developer@gmail.com>
2025-09-13 16:03:01 -0300

    first commit

---
+++ src/main/java/com/modern/jfx/spring/ModernJfxSpringApplication.java
@@ -0,0 +1,14 @@
+package com.modern.jfx.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernJfxSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernJfxSpringApplication.class, args);
+		System.out.println("Application started");
+	}
+
+}
---
+++ src/test/java/com/modern/jfx/spring/ModernJfxSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modern.jfx.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernJfxSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
