2687b122e91b2a59d7a1ca8171bc84e78d1971fb
Vinícius Martins Pedro <vinicius.martins.developer@gmail.com>
2025-09-21 10:34:48 -0300

    fix: correção dos testes

--- src/test/java/com/modern/jfx/spring/controller/UsuarioControllerIntegrationTest.java
+++ src/test/java/com/modern/jfx/spring/controller/UsuarioControllerIntegrationTest.java
@@ -17,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@org.springframework.test.context.ActiveProfiles("test")
 public class UsuarioControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
